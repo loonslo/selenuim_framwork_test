a61c0be6425737329c2c60bb5c3a9fe5cd2752e0
so <chongqing115@126.com>
2020-06-11 00:43:36 +0800

    TODO：对screenshot添加时间表示

--- LoginModule/base.py
+++ LoginModule/base.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# encoding:utf-8
 from selenium import webdriver
 import time
 import configparser
@@ -8,18 +8,29 @@ import os
 class BaseWebdriver(object):
 
     def __init__(self):
-        driver_path = os.path.dirname(os.path.dirname(__file__)) + '/DriverDir/chromedriver.exe'
+        self.by_list = ["xpath", "link text", "partial link text", "name", "tag name", "class name", "css selector",
+                        "id"]
+        driver_path = os.path.dirname(os.path.dirname(__file__)) + '/DriverDir/chromedriver'
         self.driver = webdriver.Chrome(driver_path)
 
     def get(self, url):
         return self.driver.get(url)
 
     def element(self, name):
-        by_list = ["xpath", "link text", "partial link text", "name", "tag name", "class name", "css selector", "id"]
-        for by in by_list:
+        for by in self.by_list:
             if self.driver.find_elements(by, name):
-                print(by)
+                # print(by)
                 return self.driver.find_element(by, name)
+
+    def is_exist(self, name):
+        ele_list = [self.driver.find_elements(i, name) for i in self.by_list]
+        for i in ele_list:
+            if len(i) > 0:
+                return True
+        return False
+
+    def get_screenshot(self):
+        self.driver.save_screenshot('1.png')
 
 
 class ConfigParser(object):
@@ -40,8 +51,3 @@ class ConfigParser(object):
 
     def get_user_info(self, option):
         return self.config.get('User', option)
-
-
-conf = ConfigParser()
-s = conf.get_user_info('username')
-print(type(s))
--- LoginModule/login.py
+++ LoginModule/login.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# encoding:utf-8
 import time
 
 from LoginModule.base import BaseWebdriver, ConfigParser
@@ -12,9 +12,10 @@ driver.element(config.get_element('account_button')).click()
 driver.element(config.get_element('user_name')).send_keys(config.get_user_info('username'))
 driver.element(config.get_element('user_pass')).send_keys(config.get_user_info('password'))
 
-print(config.get_element('login_button'))
-# driver.element(ConfigParser().get_element('login_button')).click()
-print(type('//button[text()="登录"]'))
 driver.element('//button[text()="登录"]').click()
-
-
+time.sleep(3)
+if driver.is_exist(config.get_element('alert-title')):
+    print('验证通过')
+else:
+    driver.get_screenshot()
+    print('验证失败')
